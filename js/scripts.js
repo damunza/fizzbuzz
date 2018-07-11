00dc494cffebcf4d4338baa428c1007845dc06c9
damunza <danidamunza@gmail.com>
2018-07-11 17:54:02 +0300

    fizz buzz working on console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-alert("hello you");
+$(document).ready(function(){
+  $("form#slap").submit(function(event){
+    event.preventDefault();
+    var userInput=parseInt($("input#number").val());
+    var result=fuzz(userInput);
+    // $("#result").text(result);
+    console.log(userInput);
+  })
+})
+
+ var fuzz=function(userInput){
+   if (userInput % 3===0 && userInput % 5===0){
+     console.log("fizzbuzz");
+} else if (userInput % 3 ===0) {
+  console.log("fizz");
+}else if (userInput % 5 ===0) {
+  console.log("buzz");
+}else {
+  console.log(userInput);
+}
+}
+//   if (userInput % 3===0) {
+//     $("#result").text("fizz");
+//   }else {
+//     $("#result").text(userInput);
+//   }
+//
+// };
